5b71206c17ca8e6dcd20c23df0d3aa76ab595986
dniro12 <dniro12@gmail.com>
2010-04-18 16:52:31 -0700

    Test.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,7 +3,7 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-
+  test
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'ffff786e90bc6ab13b0fbfc3eedf8a19'
